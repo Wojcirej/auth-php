ffbde43652431625d6ffc921b38f3739ac9c397d
Wojcirej <wojcirej@gmail.com>
2017-04-23 01:07:12 +0200

    Simple index page, bootstrap inclusion.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <meta charset="utf-8">
+  <title>Main page</title>
+</head>
+<body>
+  <div class="container">
+    <div class="page-header">
+      <p>Index page</p>
+    </div>
+  </div>
+</body>
+</html>
